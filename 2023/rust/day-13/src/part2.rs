185b7ab5a3e4c7538099067e79e21d1a10258392
OdinSrc <OdinSrc@proton.me>
2023-12-16 10:02:00 +0530

    cleanup

--- 2023/rust/day-13/src/part2.rs
+++ 2023/rust/day-13/src/part2.rs
@@ -1,9 +1,65 @@
-use std::fmt::Display;
+use std::{collections::VecDeque, fmt::Display};
 
-pub fn run(_input: &str) -> impl Display {
-    let result = "";
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum Tile {
+    Ash,
+    Rock,
+}
 
-    result
+fn parse(input: &str) -> Vec<VecDeque<Vec<Tile>>> {
+    input
+        .split("\n\n")
+        .map(|block| {
+            block
+                .lines()
+                .map(|line| {
+                    line.chars()
+                        .map(|c| match c {
+                            '.' => Tile::Ash,
+                            '#' => Tile::Rock,
+                            _ => unreachable!("Invalid Tile"),
+                        })
+                        .collect()
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn reflects_at(grid: &VecDeque<Vec<Tile>>) -> Option<usize> {
+    (1..grid.len()).find(|&offset| {
+        let half1 = grid.iter().take(offset).rev();
+        let half2 = grid.iter().skip(offset);
+        let combined = half1.zip(half2); // the shortest half determines how long this is!
+        let differences: usize = combined
+            .map(|(row1, row2)| row1.iter().zip(row2.iter()).filter(|(a, b)| a != b).count())
+            .sum();
+
+        differences == 1
+    })
+}
+
+pub fn run(input: &str) -> impl Display {
+    let grid = parse(input);
+    grid.iter()
+        .map(|grid| {
+            // check horizontal
+            if let Some(i) = reflects_at(grid) {
+                return i * 100;
+            }
+
+            // check vertical
+            let cols = (0..grid[0].len())
+                .map(|i| grid.iter().map(|row| row[i]).collect())
+                .collect();
+            if let Some(i) = reflects_at(&cols) {
+                return i;
+            }
+
+            // no reflection found
+            0
+        })
+        .sum::<usize>()
 }
 
 use crate::custom_error::AocError;
@@ -18,8 +74,22 @@ mod tests {
 
     #[test]
     fn test_solution() -> miette::Result<()> {
-        let input = "";
-        // assert_eq!("", process(input)?);
+        let input = "#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#";
+        assert_eq!("400", process(input)?);
         Ok(())
     }
 }
